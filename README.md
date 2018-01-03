612a6a35166e16b8da266b267f34bb7a53d90a3e
six-k <ramsri.hp@gmail.com>
2018-01-04 04:23:58 +0530

    First working proto
    dtreematch compares the contents of two directory trees recursively
    to check if they are similar. The paths can also be mount points.
    This is an attempt at quickly figuring out if the contents of two paths
    are not similar.
    
    The match(comparison) happens in this order for an entry(file,dir) in
    the path:
    
      - File pathname check - file availability
      - File type/format check - regular file, directory, block, link, etc.
      - File size check
      - Checksum check
    
    If any of the above check fails for an entry while traversing(the very
    first mismatch), without continuing any further, the program bails out
    saying 'FAIL' along with the mismatch or program failure reason. If
    the contents are similar, exits with a string saying 'MATCH'.
    
    Signed-off-by: six-k <ramsri.hp@gmail.com>

---
+++ README.md
@@ -0,0 +1,37 @@
+# dtreematch (WIP: prototype stage)
+
+__dtreematch__ compares the contents of two directory trees recursively to check if they are similar. The paths can also be mount points.
+This is an attempt at quickly figuring out if the contents of two paths are not similar.
+
+The match(comparison) happens in this order for an entry(file,dir) in the path:
+
+  - File pathname check - file availability
+  - File type/format check - regular file, directory, block, link, etc.
+  - File size check
+  - Checksum check
+
+If any of the above check fails for an entry while traversing(the very first mismatch), without continuing any further, the program bails out saying __'FAIL'__ along with the mismatch or program failure reason. If the contents are similar, exits with a string saying __'MATCH'__.
+
+NOTE: Since we bail out on the very first mismatch, dtreematch can not give a full list of the differences between the paths and it's not meant for that(not yet).
+
+The proto works but hasn't been tested for all cases yet. Suggestions, issues, remarks and any sort of contributions from you is most welcome. 
+
+#### Dependencies
+GCC & GLib-2.0
+
+#### To run the pre compiled binary, clone or fork this repo and `cd` to the directory:
+`./dtreematch /path/to/src /path/to/target`
+
+#### To compile:
+`gcc -Wall -Wextra -o dtreematch dtreematch.c $(pkg-config --cflags --libs glib-2.0)`
+
+#### TODO:
+  - Multithread or fork with IPC or singals
+  - Handle `strnxxx` of `string.h` stuff to avoid buffer overruns 
+  - Have input flags(switches) for configuring the comparison/check criteria
+  - Checksum the first few bytes of the files before proceeding to do it for the entire file
+  - Better exit/error reporting
+  - Verbose output
+  - Extend as a library
+  - Make it portable?
+  - Support for SSH?
---
+++ dtreematch.c
@@ -0,0 +1,169 @@
+#define _XOPEN_SOURCE 600
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <inttypes.h>
+#include <string.h>
+#include <ftw.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <glib.h>
+
+#define CHECKSUM_G G_CHECKSUM_MD5
+
+static char *PATH1;
+static char *PATH2;
+
+gchar *get_file_checksum(const char *file_path, GChecksumType checksum_type_g)
+{
+        int rfd;
+        ssize_t snr;
+        char rbuf[4096];
+        rfd = open(file_path, O_RDONLY | O_NOFOLLOW | O_NOATIME);
+        if (rfd == -1) {
+                perror("open");
+                return NULL;
+        }
+
+        GChecksum *cksum_g = NULL;
+        cksum_g = g_checksum_new(checksum_type_g);
+        gchar *hash_str;
+        if (cksum_g == NULL) {
+                fprintf(stderr, "g_checksum_new returned NULL\n");
+                return NULL;
+        }
+
+        while (pread(rfd, NULL, (size_t) 1, lseek(rfd, 0, SEEK_CUR))) {
+                if ((snr = read(rfd, &rbuf, (ssize_t) 4096)) != -1) {
+                        if (snr == 0)
+                                break;
+                        g_checksum_update(cksum_g, (guchar *) rbuf, (gssize) snr);
+                } else {
+                        perror("read");
+                        return NULL;
+                }
+        }
+        hash_str = g_strdup(g_checksum_get_string(cksum_g));
+        close(rfd);
+        g_checksum_free(cksum_g);
+
+        return hash_str;
+}
+
+static int compare_path(const char *src_path, const struct stat *sbuf1, int type)
+{
+        struct stat sbuf2;
+        const char *diff_path;
+        char *tgt_path;
+
+        diff_path = src_path + strlen(PATH1);
+        tgt_path = calloc(1, strlen(PATH2) + strlen(diff_path) + 1);
+        if (tgt_path == NULL) {
+                perror("calloc");
+                return -1;
+        }
+
+        tgt_path = strncpy(tgt_path, PATH2, strlen(PATH2) + 1);
+        tgt_path = strncat(tgt_path, diff_path, strlen(diff_path));
+
+        if (lstat(tgt_path, &sbuf2) == -1) {
+                perror("stat");
+                fprintf(stderr, "FAIL: file missing [%s, %s]\n", src_path, tgt_path);
+                return -1;
+        }
+
+        if (!((sbuf1->st_mode & S_IFMT) == (sbuf2.st_mode & S_IFMT))) {
+                fprintf(stderr, "FAIL: file format differs [%s, %s]\n", src_path, tgt_path);
+                return -1;
+        }
+        if (type == FTW_F) {
+                if (!(sbuf1->st_size == sbuf2.st_size)) {
+                        fprintf(stderr, "FAIL: file size differs [%s, %s]\n", src_path, tgt_path);
+                        return -1;
+                }
+
+                char *cksum1 = NULL;
+                char *cksum2 = NULL;
+                cksum1 = (char *) get_file_checksum(src_path, CHECKSUM_G);
+                if (cksum1 == NULL)
+                        return -1;
+                cksum2 = (char *) get_file_checksum(tgt_path, CHECKSUM_G);
+                if (cksum2 == NULL)
+                        return -1;
+
+                if (strcmp(cksum1, cksum2)) {
+                        fprintf(stderr, "FAIL: file checksum differs [%s, %s]\n", src_path, tgt_path);
+                        return -1;
+                }
+        }
+        free(tgt_path);
+        return 0;
+}
+
+static int dtree_check(const char *path, const struct stat *sbuf, int type,
+                        struct FTW *ftwb)
+{
+	switch(type) {
+	case FTW_D:
+		if (ftwb->level == 0)
+			return FTW_CONTINUE;
+	case FTW_F:
+	case FTW_SL:
+                /*
+                fprintf(stdout, "%-14ld::%-4ld::%-40s::%-14jd::%d::%s\n",
+				(long) sbuf->st_ino,
+				(long) sbuf->st_nlink,
+				path,
+				(intmax_t) sbuf->st_size,
+				ftwb->level,
+				path + ftwb->base);
+                */
+                if (compare_path(path, sbuf, type))
+                        return FTW_STOP;
+		return FTW_CONTINUE;
+	default:
+		fprintf(stderr, "Unexpected situation, exiting!\n");
+		return FTW_STOP;
+	}
+
+}
+
+int main(int argc, char *argv[])
+{
+	PATH1 = realpath((argc > 1) ? argv[1] : ".", NULL);
+	if (PATH1 == NULL) {
+		perror("realpath");
+		exit(EXIT_FAILURE);
+	}
+	PATH2 = realpath((argc > 2) ? argv[2] : ".", NULL);
+	if (PATH2 == NULL) {
+		perror("realpath");
+		exit(EXIT_FAILURE);
+	}
+
+        int ftw_flags;
+        ftw_flags               = FTW_PHYS  |
+                                  /* FTW_MOUNT | */
+                                  FTW_ACTIONRETVAL;
+
+        int nftw_ret;
+        nftw_ret = nftw(PATH1, dtree_check, 30, ftw_flags);
+	if (nftw_ret == -1) {
+		perror("nftw");
+		exit(EXIT_FAILURE);
+	} else if (nftw_ret == FTW_STOP) {
+                fprintf(stdout, "FAIL\n");
+		exit(EXIT_FAILURE);
+	} else if (nftw_ret == 0) {
+                fprintf(stdout, "MATCH\n");
+	}
+
+        free(PATH1);
+        free(PATH2);
+
+        exit(EXIT_SUCCESS);
+}
